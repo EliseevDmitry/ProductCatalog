669443eb7dd12bde39cee3a724a582da94cb0fa1
Dmitry <dimasel@gmail.com>
2024-10-28 11:36:52 +0700

    New project

---
+++ ProductCatalog/ProductCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCatalogApp.swift
+//  ProductCatalog
+//
+//  Created by Dmitriy Eliseev on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProductCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
